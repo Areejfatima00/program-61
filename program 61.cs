c7f2f843e0fd8beeab74efa7677df289b2e1028c
Areej Fatima <areej4761@gmail.com>
2021-11-27 22:22:22 +0500

    first commit

---
+++ program 61.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace question_61
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 22 / 8;
+            Console.WriteLine(n);
+            Console.ReadLine();
+
+
+            int a = 99 / 8 + 21 / 11 * 7;
+            Console.WriteLine(a);
+            Console.ReadLine();
+
+
+
+
+            double c = 4, d = 3, p = 8, q = 21;
+            double t = q / c - p / d;
+            Console.WriteLine(t);
+            Console.ReadLine();
+        }
+    }
+}
